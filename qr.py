9657274e1bc80a25e9f9f2f2ebf1958e73a0adcc
dawitstranger@gmail.com <dawitk27@gmail.com>
2025-07-25 03:38:13 +0200

    updated text function

--- qr.py
+++ qr.py
@@ -1,20 +1,50 @@
-from libraries.available_experiments import propagate_others
+import sys
+"""
+This script is a quick test for SelfPhish, which runs a simulation and visualizes the results.
+You can run it directly after setting up the environment, or copy it to your working directory and adjust the paths accordingly.
+Make sure to have the necessary libraries installed and the paths set correctly.
+To run the script, simply execute:
+```bash
+python qr.py
+```
+You can modify the parameters in the `sim_bubbles_prop` function to suit your specific experiments.
+Example:
+
+- image size: 256x256
+- number of bubbles: 10
+- fresnel number: 1e-3
+- positive phase: 'relu_inverted'
+- positive attenuation: 'relu'
+- absolute ratio: 1e-2
+- downsampling factor: 1
+
+This will generate a simulation with the specified parameters, train the model for 200 iterations, and visualize the results in a grid format.
+"""
+path_to_git = 'SelfPhish/libraries' if 'SelfPhish' in sys.path else '/data/hereon/wp/user/hailudaw/git_folders/paper_selfphish/SelfPhish/libraries'
+if 'SelfPhish' not in sys.path:
+    sys.path.append(path_to_git)
+else:
+    assert sys.path[0] == path_to_git, "Path to SelfPhish is not set correctly."
+
+from libraries.available_experiments import sim_bubbles_prop
 from libraries.gan4inverse import solver
-from libraries.utils import generate_qr_code
-from libraries.visualize import visualize
-import torch
 
-# Example usage
-image_path='data/qr/'
-img = generate_qr_code("Phase (V20|BS10|B4): A Good Phase: Solving the Near-field Phase Retrieval Inverse Problem Quantitatively Using SelfPhish. Phase retrieval is a non-linear, ill-posed inverse problem that is essential for accurate wavefront reconstruction prior to tomographic analysis. SelfPhish enables quantitative phase re-trieval with high precision. ", image_path+'phase_qr_code.tif', 10)
-img2 = generate_qr_code("Absorbance: Experimental results demonstrate SelfPhish's superior accuracy and robustness across diverse imaging scenarios. underscoring its potential to significantly enhance phase retrieval and image quality in X-ray imaging and beyond.", image_path+'abs_qr_code.tif', 10)
-
-fresnel_prop = propagate_others(phase = img, attenuation = img2, fresnel_number =  10**(-2.5), task = 'learn_phase', dsf=2**0, positive_attenuation = 'relu', positive_phase = 'relu_inverted', transformation_type='ssim', abs_ratio = 1, model_type = 'wavelet', wavelet = 'db1', init_model = False, save_model_path = image_path, save_model = True, device = 'cuda:0', add_noise = False, noise_factor = 0.01, noise_type = 'gaussian', dis_depth = 2, dis_type = 'cnn')
-visualize([fresnel_prop['transformed_images'], torch.abs(fresnel_prop['wavefield']), torch.abs(fresnel_prop['fresnel_factor'].imag)], title = ['input', 'wavefield', 'fresnel_factor'],  cmap = 'coolwarm', mode = 'show', axis = 'off', plot_axis = 'half', images_per_row=8, fig_size=(20,10), min_max = False)
-fresnel_prop['device'] = 'cuda:1'
-ssim_list, ground_ssim_list, ground_att_ssim_list = [], [], []
-psnr_list, ground_psnr_list, ground_att_psnr_list = [], [], []
-comparison = []
-model = solver(**fresnel_prop)
+sim = sim_bubbles_prop(256, 256, 10, 
+    fresnel=5e-3, 
+    positive_attenuation='relu',
+    positive_phase='relu_inverted',
+    transformation_type='reshape',
+    abs_ratio=1,
+    model_type='wavelet',
+    init_model=False,
+    device='cuda:0',
+    add_noise=False,
+    noise_factor=0.01,
+    noise_type='gaussian',
+    dis_type='cnn',
+    dis_depth=2,
+    dsf=1,
+)
+model = solver(**sim)
 model.train(iter_num = 400)
-model.visualize(cmap = 'gray', vmode = 'zoom', axis = 'off', plot_axis = 'half', images_per_row=8, fig_size=(20,10), min_max = False)
+model.visualize(images_per_row=3, vmode = 'show', title_fontsize = 5, colorbar = False, axis = 'off')
